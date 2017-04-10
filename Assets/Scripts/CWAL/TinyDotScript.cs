9fa57363f18390aa7df501578cf61be2c218cc17
erackley <erackley@malwarebytes.org>
2017-04-09 22:19:18 -0400

    * Initial check in!

---
+++ Assets/Scripts/CWAL/TinyDotScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TinyDotScript : MonoBehaviour {
+
+    public float leftSpawnPoint = -15f;
+    public float rightSpawnPoint = 15f;
+    
+    public float minSize = 0.15f;
+    public float maxSize = 0.30f;
+    public float minLife = 5.0f;
+    public float maxLife = 40.0f;
+
+    private float lifeUsed = 0.0f;
+    private float lifeAllowed = 40.0f;
+
+    void setupDot()
+    {
+        //Vector3 startingPostion = new Vector3(Random.Range(0.3f, 2.5f), Random.Range(7f, 8f), 0.0f);
+        //works with cwal proto Vector3 startingPostion = new Vector3(Random.Range(2.15f, 2.2f), Random.Range(9f, 20f), 0.0f);
+        Vector3 startingPostion = new Vector3(Random.Range(leftSpawnPoint, rightSpawnPoint), Random.Range(9f, 30f), 0.0f);
+        this.transform.position = startingPostion;
+        float size = Random.Range(minSize, maxSize);
+        Vector3 newScale = new Vector3(size, size, 1.0f);
+        gameObject.transform.localScale = newScale;
+        gameObject.GetComponent<Rigidbody>().mass = size;
+        lifeUsed = 0.0f;
+        lifeAllowed = Random.Range(minLife, maxLife);
+
+        //Color currentColor = this.gameObject.GetComponent<SpriteRenderer>().color;
+        //currentColor.a = 0f;
+        //this.gameObject.GetComponent<SpriteRenderer>().color = currentColor;
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        setupDot();
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        lifeUsed += Time.deltaTime;
+        if(lifeUsed > lifeAllowed || this.transform.position.y <= -22.0f)
+        {
+            setupDot();
+        }
+
+        //Color currentColor = this.gameObject.GetComponent<SpriteRenderer>().color;
+        //if(currentColor.a < 1f)
+        //{
+        //    currentColor.a += Time.deltaTime / 2f;
+        //}
+        //this.gameObject.GetComponent<SpriteRenderer>().color = currentColor;
+
+    }
+}
---
+++ Assets/Scripts/JointTracker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class JointTracker : MonoBehaviour {
+
+    public JointType JointToUse;
+    public BodySourceManager _bodyManager;
+    public float scale = 8f;
+    public float yOffset = -5f;
+    public int BodyNumber = 0;
+
+    private TrackerDot m_trackerDot;
+    private float m_trackerStartingY;
+
+    void Awake()
+    {
+        m_trackerDot = FindObjectOfType<TrackerDot>();
+        m_trackerStartingY = m_trackerDot.transform.position.y;
+    }
+
+    // Get body data from the body manager and track the joint for the active body
+    void Update()
+    {
+        if (_bodyManager == null)
+        {
+            return;
+        }
+
+        Body[] data = _bodyManager.GetData();
+        if (data == null)
+        {
+            return;
+        }
+
+        if((data.Length >= BodyNumber) && (data[BodyNumber] != null) && (data[BodyNumber].IsTracked))
+        {
+            var pos = data[BodyNumber].Joints[JointToUse].Position;
+            float yPosition = (pos.Y * scale) + (yOffset + (m_trackerDot.transform.position.y - m_trackerStartingY));
+            this.gameObject.transform.position = new Vector3(pos.X * scale, yPosition, 0f);
+        }
+        else
+        {
+            this.gameObject.transform.position = new Vector3(-30f, 0f, 0f);
+        }
+
+    }
+
+}
