ca364ee8e80555603a479a3e7d2402666bbea03c
erackley <erackley@malwarebytes.org>
2017-04-12 14:30:15 -0400

    * Trying to get 2d model to work with kinect, 'not sure it'll work.

--- Assets/Scripts/JointTracker.cs
+++ Assets/Scripts/JointTracker.cs
@@ -10,6 +10,7 @@ public class JointTracker : MonoBehaviour {
     public float scale = 8f;
     public float yOffset = -5f;
     public int BodyNumber = 0;
+    public GameObject PointToward;
 
     private TrackerDot m_trackerDot;
     private float m_trackerStartingY;
@@ -34,24 +35,47 @@ public class JointTracker : MonoBehaviour {
             return;
         }
 
-        if((data.Length >= BodyNumber) && (data[BodyNumber] != null) && (data[BodyNumber].IsTracked))
+        // Use for testing with a single body model to make life easier
+        foreach(Body body in data)
         {
-            GetComponent<Rigidbody>().isKinematic = true;
-            var pos = data[BodyNumber].Joints[JointToUse].Position;
-            float yPosition = (pos.Y * scale) + (yOffset + (m_trackerDot.transform.position.y - m_trackerStartingY));
-            Vector3 targetPosition = new Vector3(pos.X * scale, yPosition, 0f);
-            this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, Time.deltaTime * 10);
-            //this.transform.position = targetPosition;
+            if ((body != null) && (body.IsTracked))
+            {
+                var pos = body.Joints[JointToUse].Position;
+                //float yPosition = (pos.Y * scale) + (yOffset + (m_trackerDot.transform.position.y - m_trackerStartingY));
+                Vector3 targetPosition = new Vector3(pos.X * scale, yOffset + pos.Y * scale, 0f);
+                //this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, Time.deltaTime * 10);
+                this.transform.position = targetPosition;
+                Quaternion rotation;
+                rotation.x = body.JointOrientations[JointToUse].Orientation.X;
+                rotation.y = body.JointOrientations[JointToUse].Orientation.Y;
+                rotation.z = body.JointOrientations[JointToUse].Orientation.Z;
+                rotation.w = body.JointOrientations[JointToUse].Orientation.W;
+                Vector3 eulerRotation = rotation.eulerAngles;
+                Vector3 limitedRotation = new Vector3(0f, 0f, -eulerRotation.z);
+                transform.rotation = Quaternion.Euler(limitedRotation);
+                //var rotation = Quaternion.LookRotation(lookPos);
+                //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+            }
+        }
 
-            // Try moving to target vector with physics
-            // Vector3 dir = (targetPosition - transform.position).normalized * 5f;
-            // this.GetComponent<Rigidbody>().velocity = dir;
-        }
-        else
-        {
-            GetComponent<Rigidbody>().isKinematic = false;
-            //this.gameObject.transform.position = new Vector3(-30f, 0f, 0f);
-        }
+        //if((data.Length >= BodyNumber) && (data[BodyNumber] != null) && (data[BodyNumber].IsTracked))
+        //{
+        //    GetComponent<Rigidbody>().isKinematic = true;
+        //    var pos = data[BodyNumber].Joints[JointToUse].Position;
+        //    float yPosition = (pos.Y * scale) + (yOffset + (m_trackerDot.transform.position.y - m_trackerStartingY));
+        //    Vector3 targetPosition = new Vector3(pos.X * scale, yPosition, 0f);
+        //    //this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, Time.deltaTime * 10);
+        //    this.transform.position = targetPosition;
+        //
+        //    // Try moving to target vector with physics
+        //    // Vector3 dir = (targetPosition - transform.position).normalized * 5f;
+        //    // this.GetComponent<Rigidbody>().velocity = dir;
+        //}
+        //else
+        //{
+        //    GetComponent<Rigidbody>().isKinematic = false;
+        //    //this.gameObject.transform.position = new Vector3(-30f, 0f, 0f);
+        //}
 
     }
 
